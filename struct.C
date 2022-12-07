51816431f85b604bf8cd6cd0c9598a17bbe63428
Peixoto <arthurpf@id.uff.br>
2022-12-07 12:29:06 -0300

    ex.1 struct atualizado

--- struct.C
+++ struct.C
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main (){
-    struct aluno
+
+ typedef struct aluno
     {
         int matricula;
         int notap2;
         int notap1;
-        char curso[10];
+        char curso[25]; 
     };
-    struct aluno jose;
-    fgets(jose.curso,10,stdin);
+
+int main (){
+    
+    struct aluno  jose ;
+    {
+    printf("Digite o curso do jose:");
+    fgets(jose.curso,25,stdin);
     jose.matricula = 222060075;
     jose.notap1 = 10;
-    jose.notap2 = 7;
+    jose.notap2 = 7; 
+    };
 
     float media = (jose.notap1 + jose.notap2) / 2;
-    printf("a media do aluno %d e media: %.2f", jose.matricula, media);
+
+    printf("\n\nO aluno cursa %s\n\n", jose.curso);
+
+    printf("a media do aluno %d e media: %.2f z\n\n", jose.matricula, media);
+
     
+
     return 0;
 }
